0cc7f5bfc826172aa258d9857f80665e560e66cc
BaTru <batru96@github.com>
2018-01-24 19:44:21 +0700

    Add swiper

---
+++ src/components/Main.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { View, Image, StyleSheet, Dimensions } from 'react-native';
+import Swiper from 'react-native-swiper';
+import Header from './header/Header';
+import pic1 from '../icons/1.jpg';
+import pic2 from '../icons/2.jpg';
+import pic3 from '../icons/3.jpg';
+
+const { width } = Dimensions.get('window');
+export default class Main extends Component {
+    render() {
+        const { container, imageSwiper, swiper } = styles;
+        return (
+            <View style={container}>
+                <Header />
+                <View style={swiper}>
+                    <Swiper showButtons={false}>
+                        <Image source={pic1} style={imageSwiper} />
+                        <Image source={pic2} style={imageSwiper} />
+                        <Image source={pic3} style={imageSwiper} />
+                    </Swiper>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    swiper: {
+        height: (width * 354) / 800
+    },
+    imageSwiper: {
+        flex: 1,
+        width,
+        alignItems: 'center'
+    }
+});
---
+++ src/components/header/Header.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import icMenu from '../../icons/ic_menu_black.png';
+
+export default class Header extends Component {
+    onPress() {
+
+    }
+
+    render() {
+        const { container, title } = styles;
+        return (
+            <View style={container}>
+                <TouchableOpacity onPress={this.onPress.bind(this)}>
+                    <Image source={icMenu} />
+                </TouchableOpacity>
+                <Text style={title}>SIMPLE SHOPPING</Text>
+                <View />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 8,
+        backgroundColor: 'lightblue',
+    },
+    title: {
+        fontWeight: 'bold'
+    }
+});
